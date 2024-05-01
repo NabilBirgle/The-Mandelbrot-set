487f2efcbb08d972760658e158196dfb81b968db
Nabil Birgle <nabil.birgle@gmail.com>
2024-05-01 16:52:29 +0200

    Initial Commit

---
+++ Mandelbrot/MandelbrotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandelbrotApp.swift
+//  Mandelbrot
+//
+//  Created by Nabil Birgle on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MandelbrotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
